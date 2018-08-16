734f2ec5db8581c046a0b1cd949772facdc88624
Saúl Piña <sauljabin@gmail.com>
2018-08-16 13:47:41 -0500

    Adding logs generator app

---
+++ logs-generator/src/main/java/app/LogTask.java
@@ -0,0 +1,28 @@
+package app;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.TimerTask;
+
+public class LogTask extends TimerTask {
+    public final List<String> levels = Arrays.asList(
+            "INFO",
+            "ERROR"
+    );
+    public final Logger log = LogManager.getLogger(LogTask.class);
+    public final Random random = new Random();
+
+    @Override
+    public void run() {
+        log.log(getRandomLevel(), "request");
+    }
+
+    private Level getRandomLevel() {
+        return Level.getLevel(levels.get(random.nextInt(levels.size())));
+    }
+}
---
+++ logs-generator/src/main/java/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+import java.util.Timer;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new LogTask(), 0, 1000);
+    }
+}
