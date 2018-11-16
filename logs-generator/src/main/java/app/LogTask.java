351c5996b862103a49acff1b515dc7b58fd78b3a
Saúl Piña <sauljabin@gmail.com>
2018-11-15 20:35:07 -0500

    Adding new keys to logs generator

--- logs-generator/src/main/java/app/LogTask.java
+++ logs-generator/src/main/java/app/LogTask.java
@@ -3,22 +3,34 @@ package app;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.TimerTask;
 
 public class LogTask extends TimerTask {
-    public static final String MESSAGE = "complete={} status={}";
-    public final Logger log = LogManager.getLogger(LogTask.class);
-    public final Random random = new Random();
 
-    @Override
-    public void run() {
-        int rand = random.nextInt(100);
-        if (rand < 95) {
-            log.info(MESSAGE, "success", "200");
-        } else {
-            log.error(MESSAGE, "fail", "500");
-        }
-    }
+	public static final String MESSAGE_INFO = "complete={} service={} status={} response='{}'";
+	public static final String MESSAGE_ERROR = "complete={} service={} status={} code={} response='{}'";
+
+	public final Logger log = LogManager.getLogger(LogTask.class);
+	public final Random random = new Random();
+	public final List<String> services = Arrays.asList("invoice", "payroll", "user", "accounting", "purchasing", "sale");
+	public final List<String> errors = Arrays.asList("400:A001:Bad request", "500:B002:Exception null pointer exeption", "403:C050:Unauthorized token", "500:B003:Dependent records not completed");
+	public final List<String> success = Arrays.asList("200:Operation success", "201:Saved record", "204:Operation success", "404:Not found");
+
+	@Override
+	public void run() {
+		int rand = random.nextInt(100);
+		String service = services.get(random.nextInt(services.size()));
+
+		if (rand < 90) {
+			String[] info = success.get(random.nextInt(success.size())).split(":");
+			log.info(MESSAGE_INFO, "success", service, info[0], info[1]);
+		} else {
+			String[] error = errors.get(random.nextInt(errors.size())).split(":");
+			log.error(MESSAGE_ERROR, "fail", service, error[0], error[1], error[2]);
+		}
+	}
 
 }
--- logs-generator/src/main/java/app/Main.java
+++ logs-generator/src/main/java/app/Main.java
@@ -3,8 +3,8 @@ package app;
 import java.util.Timer;
 
 public class Main {
-    public static void main(String[] args) {
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new LogTask(), 0, 1000);
-    }
+	public static void main(String[] args) {
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new LogTask(), 0, 500);
+	}
 }
